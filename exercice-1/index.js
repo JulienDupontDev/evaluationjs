eff36cf6cb6c5447adb19e2c2c0971a4b1065a88
Julien Dupont <julien.dupont2018@gmail.com>
2020-10-12 10:46:22 +0200

    feat(exercice1): added exercice 1

---
+++ exercice-1/index.js
@@ -0,0 +1,37 @@
+'use strict';
+
+window.onload = function(){
+    gridGenerator(5,3);
+} 
+
+function gridGenerator(xAxis,yAxis){
+    let table = document.createElement("table");
+    for(let y=0;y<yAxis;y++){
+        let tr = document.createElement("tr");
+        tr.style.padding = "25px";
+
+        for(let x=0;x<xAxis;x++){
+           let td = document.createElement("td");
+            td.style.width = "30px";
+    td.style.height = "30px";
+           setInterval(function(){
+                td.style.backgroundColor = generateColor();
+               tr.appendChild(td);
+           }, Math.random() * (2000 - 1000) + 1000)
+        }
+        table.appendChild(tr);
+    }
+    document.querySelector('body').appendChild(table);
+}
+
+var generateColor = function () {
+
+    var letters = '0123456789ABCDEF';
+  var color = '#';
+  for (var i = 0; i < 6; i++) {
+    color += letters[Math.floor(Math.random() * 16)];
+  }
+  return color;
+
+};
+
